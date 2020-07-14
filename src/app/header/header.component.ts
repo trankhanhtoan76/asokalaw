4ee394fcebbcd8a87021dc03701fb33804ce8eb4
tkt <toan.tran@dotb.vn>
2020-07-14 08:56:43 +0700

    f

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import {Component, HostListener, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
