7f3a637430623bdc7b7112de1612228aea013a2a
tkt <toan.tran@dotb.vn>
2020-08-01 03:35:40 +0700

    form dknh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {NavigationEnd, Router} from '@angular/router';
+import {SpinnerService} from "./service/spinner.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router) {
+  constructor(private router: Router,private spinner: SpinnerService) {
+      const self = this;
+      self.spinner.show('Loading');
+      // tslint:disable-next-line:only-arrow-functions
+      window.onload = function() {
+          self.spinner.hide();
+      };
   }
 
   ngOnInit(): void {
