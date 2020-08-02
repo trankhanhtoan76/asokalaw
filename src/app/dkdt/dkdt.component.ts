8041248f0125d5571ba679cc79188daeeba63c25
tkt <toan.tran@dotb.vn>
2020-08-03 00:35:21 +0700

    form

--- src/app/dkdt/dkdt.component.ts
+++ src/app/dkdt/dkdt.component.ts
@@ -31,6 +31,39 @@ export class DkdtComponent implements OnInit {
     ngOnInit(): void {
     }
 
+    fieldValid(value, type?): boolean {
+        if (value) {
+            if (type == 'phone') {
+                return /^[0-9]{8,15}$/.test(value);
+            } else if (type == 'email') {
+                return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    formValid(fields?: any): boolean {
+        if (!fields) {
+            return this.fieldValid(this.name) && this.fieldValid(this.email, 'email') && this.fieldValid(this.phone, 'phone');
+        } else {
+            for (const key in fields) {
+                if (fields.hasOwnProperty('key')) {
+                    if (fields[key].hasOwnProperty('name')) {
+                        if (fields[key].hasOwnProperty('type')) {
+                            if (!this.fieldValid(fields[key].name, fields[key].type))
+                                return false;
+                        } else {
+                            if (!this.fieldValid(fields[key].name))
+                                return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
     submit(): void {
         this.spinner.show('sending');
         const body = `<table width="620" cellspacing="0" cellpadding="0" border="0" align="center"> <tbody> <tr> <td bgcolor="#f5f5f5"> <table width="578" cellspacing="0" cellpadding="0" border="0" align="center"> <tbody> <tr> <td height="16"></td> </tr> <tr> <td align="center"><img src="https://ci5.googleusercontent.com/proxy/-8AhuwEBWjbUauG2vyvW7r8a5aY2HZ9kZC00fhd_MiyExMPvQkH3XjyfGME-foH3AJigwdw1OkhD7o3RDAsPCB4=s0-d-e1-ft#https://www.asokalaw.vn/assets/images/logo.png" alt="Công ty Luật TNHH Asoka" style="width:200px" class="CToWUd"></td> </tr> <tr> <td height="16"></td> </tr> <tr> <td align="left" bgcolor="#fff"> <div style="border-style:solid;border-width:1px;border-color:#ccc"> <table width="578" cellspacing="0" cellpadding="0" border="0" align="center"> <tbody> <tr> <td height="22" colspan="3"></td> </tr> <tr> <td width="40"></td> <td width="498">
