114052a21ece77ab21ab959260b11c877256a143
tkt <toan.tran@dotb.vn>
2020-08-03 00:53:59 +0700

    form pltx

--- src/app/pltx/pltxservice/pltxservice.component.ts
+++ src/app/pltx/pltxservice/pltxservice.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+declare var $: any;
 
 @Component({
-  selector: 'app-pltxservice',
-  templateUrl: './pltxservice.component.html',
-  styleUrls: ['./pltxservice.component.css']
+    selector: 'app-pltxservice',
+    templateUrl: './pltxservice.component.html',
+    styleUrls: ['./pltxservice.component.css']
 })
 export class PltxserviceComponent implements OnInit {
+    @Output() changepackage = new EventEmitter();
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    onChangePackage(name) {
+        this.changepackage.emit(name);
+        $('#pltxform1').modal('show');
+    }
 }
