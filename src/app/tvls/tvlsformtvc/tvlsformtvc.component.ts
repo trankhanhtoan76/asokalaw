0d67d7470fddff781297fcadac5e48c8451c59de
tkt <toan.tran@dotb.vn>
2020-08-03 01:05:55 +0700

    form tvls

--- src/app/tvls/tvlsformtvc/tvlsformtvc.component.ts
+++ src/app/tvls/tvlsformtvc/tvlsformtvc.component.ts
@@ -25,6 +25,38 @@ export class TvlsformtvcComponent implements OnInit {
 
     ngOnInit(): void {
     }
+    fieldValid(value, type?): boolean {
+        if (value) {
+            if (type == 'phone') {
+                return /^[0-9]{8,15}$/.test(value);
+            } else if (type == 'email') {
+                return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    formValid(fields?: any): boolean {
+        if (!fields) {
+            return this.fieldValid(this.name) && this.fieldValid(this.email, 'email') && this.fieldValid(this.phone, 'phone');
+        } else {
+            for (const key in fields) {
+                if (fields.hasOwnProperty('key')) {
+                    if (fields[key].hasOwnProperty('name')) {
+                        if (fields[key].hasOwnProperty('type')) {
+                            if (!this.fieldValid(fields[key].name, fields[key].type))
+                                return false;
+                        } else {
+                            if (!this.fieldValid(fields[key].name))
+                                return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
 
     closeModal() {
         // @ts-ignore
