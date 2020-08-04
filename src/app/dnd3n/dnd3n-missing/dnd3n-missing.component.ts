3e872d62af72981903a16359fc4d879e0a48aa66
tkt <toan.tran@dotb.vn>
2020-08-04 22:56:17 +0700

    fix feedback 03/08

--- src/app/dnd3n/dnd3n-missing/dnd3n-missing.component.ts
+++ src/app/dnd3n/dnd3n-missing/dnd3n-missing.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+
+declare var $: any;
 
 @Component({
-  selector: 'app-dnd3n-missing',
-  templateUrl: './dnd3n-missing.component.html',
-  styleUrls: [
-    '../dnd3n.component.css',
-    './dnd3n-missing.component.css']
+    selector: 'app-dnd3n-missing',
+    templateUrl: './dnd3n-missing.component.html',
+    styleUrls: [
+        '../dnd3n.component.css',
+        './dnd3n-missing.component.css']
 })
 export class Dnd3nMissingComponent implements OnInit {
+    @Output() onselectservice = new EventEmitter;
+    s1 = 'Quản trị 4 trong 1: pháp lý - thuế - hợp đồng - lao động';
+    s2 = 'Đăng ký giấy phép ngành nghề có điều kiện';
+    s3 = 'Đăng ký nhãn hiệu';
+    s4 = 'Khác';
+    constructor() {
+    }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
+    selectService(value) {
+        this.onselectservice.emit((value));
+        $('#dnd3nform1').modal('show');
+    }
 
 }
