5dfe897f1193891cff3d54b10a2ac1bd078af523
tkt <toan.tran@dotb.vn>
2020-08-05 17:42:45 +0700

    fix feedback 03/08

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,20 @@ import {NavigationEnd, Router} from '@angular/router';
 import {SpinnerService} from "./service/spinner.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router,private spinner: SpinnerService) {
-      const self = this;
-      self.spinner.show('Loading');
-      // tslint:disable-next-line:only-arrow-functions
-      window.onload = function() {
-          self.spinner.hide();
-      };
-  }
+    constructor(private router: Router, private spinner: SpinnerService) {
+        this.spinner.hide();
+    }
 
-  ngOnInit(): void {
-    this.router.events.subscribe((evt) => {
-      if (!(evt instanceof NavigationEnd)) {
-        return;
-      }
-      window.scrollTo(0, 0);
-    });
-  }
+    ngOnInit(): void {
+        this.router.events.subscribe((evt) => {
+            if (!(evt instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0);
+        });
+    }
 }
