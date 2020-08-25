5e3cb1dbd417981f11cad4bf7bfce4dde06a9c58
caykhe36 <Anhbinhvipboy123@>
2020-08-25 20:15:46 +0700

    Dịch nửa trang chủ

--- src/lang.ts
+++ src/lang.ts
@@ -10,5 +10,532 @@ export const langDefined = {
     'chung-toi-la-asokalaw': {
         vi: "Chúng tôi là Asoka Law",
         en: "Chúng tôi là Asoka Law"
-    }
+    },
+    'hang-luat-chinh-danh-danh-cho-doanh-nghiep': {
+        vi: "Hãng luật chính danh dành cho doanh nghiệp",
+        en: "A legitimate law firm for SMEs"
+    },
+    'chon-linh-vuc-ban-quan-tam': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'doanh-nghiep': {
+        vi: "Doanh nghiệp",
+        en: "Bussiness"
+    },
+    'so-huu-tri-tue': {
+        vi: "Sở hữu trí tuệ",
+        en: "Intellectual Property"
+    },
+    'home-tranhchapthuongmai': {
+        vi: "Tranh chấp thương mại",
+        en: "Commercial Dispute"
+    },
+    'home-laodongnhansu': {
+        vi: "Lao động - nhân sự",
+        en: "Labour"
+    },
+    'home-sukien-truyenthong': {
+        vi: "Sự kiện - truyền thông",
+        en: "Event - Media & Communication"
+    },
+    'home-dangkydautu': {
+        vi: " Đăng ký đầu tư",
+        en: " Investment"
+    },
+    'home-nganhang-baohiem': {
+        vi: "Ngân hàng - bảo hiểm",
+        en: "Banking - Insurance"
+    },
+    'home-dinhcu-duhoc': {
+        vi: "Định cư - du học",
+        en: "Study & Settle Abroad"
+    },
+    'home-linhvuc-khac': {
+        vi: "Ngân hàng - bảo hiểm",
+        en: "Other Fields"
+    },
+    'nhan-tu-van-mien-phi-home': {
+        vi: "Nhận tư vấn miễn phí",
+        en: "Get free advice"
+    },
+    've-asoka-home': {
+        vi: "Về Asoka",
+        en: "About Us"
+    },
+    'phap-ly-doanh-nghiep-home': {
+        vi: "Pháp lý doanh nghiệp",
+        en: "Legal Services for Businesses"
+    },
+    'dich-vu-phap-ly-home': {
+        vi: "Dịch vụ pháp lý",
+        en: "Legal services"
+    },
+    'home-tin-tuc': {
+        vi: "Tin tức",
+        en: "News "
+    },
+    'home-khach-hang': {
+        vi: "Khách hàng",
+        en: "Clients"
+    },
+    'home-tu-van': {
+        vi: "Tư vấn",
+        en: "Book now"
+    },
+    'home-dich-vu-sd-nhieu': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-tu-van-truc-tuyen': {
+        vi: "Tư vấn trực tuyến",
+        en: "Online consultation"
+    },
+    'home-dasudung': {
+        vi: "Đã sử dụng: ",
+        en: "Used: "
+    },
+    '2358luot': {
+        vi: "2358 lượt",
+        en: "2358 times"
+    },
+    'home-gia-phi-tu': {
+        vi: "Giá phí từ: ",
+        en: "Price from: "
+    },
+    '200k': {
+        vi: "200.000 VNĐ",
+        en: "VND 200,000"
+    },
+    'home-dang-ky-kinh doanh': {
+        vi: "Đăng ký kinh doanh",
+        en: "Business registration"
+    },
+
+
+    '1236luot': {
+        vi: "1236 lượt",
+        en: "1236 times"
+    },
+    '2000k': {
+        vi: "2000.000 VNĐ",
+        en: "VND 2000,000"
+    },
+    'homme-gia-phi-tu-1900k': {
+        vi: "Giá phí từ: 1.900.000 VNĐ",
+        en: "Price from: VND 1,900,000"
+    },
+    'home-phaplythuongxuyen': {
+        vi: "Pháp lý thường xuyên",
+        en: "Regular legal services"
+    },
+    '1109luot': {
+        vi: "1.109 lượt",
+        en: "1.109 times"
+    },
+
+    'home-3tr': {
+        vi: "3.000.000 VNĐ",
+        en: "VND 3,000,000"
+    },
+
+    'home-dangkynhanhieu': {
+        vi: "Đăng ký nhãn hiệu",
+        en: "Trademark registration"
+    },
+    '2268luot': {
+        vi: "2.268 lượt",
+        en: "2.268 times"
+    },
+    'home-3tr5': {
+        vi: "3.500.000 VNĐ",
+        en: "VND 3,500,000"
+    },
+    'home-phaplysukien': {
+        vi: "Pháp lý sự kiện",
+        en: "Event-related legal services"
+    },
+    '30luot': {
+        vi: "30 lượt",
+        en: "30 times"
+    },
+    'home-6tr': {
+        vi: "6.000.000 VNĐ",
+        en: "VND 6,000,000"
+    },
+    'home-giaphitu15tr': {
+        vi: "Giá phí từ: 15.000.000 VNĐ",
+        en: "Price from: VND 15,000,000"
+    },
+    'home-giai-quyet-tranhchap': {
+        vi: "Giải quyết tranh chấp",
+        en: "Dispute resolution"
+    },
+    '218luot': {
+        vi: "218 lượt",
+        en: "218 times"
+    },
+    'home-15tr': {
+        vi: "15.000.000 VNĐ",
+        en: "VND 15,000,000"
+    },
+    'home-gia-phi-tu20tr': {
+        vi: "Giá phí từ: 20.000.000 VNĐ",
+        en: "Price from: VND 20,000,000"
+    },
+    'home-dang-ky-dich-vu': {
+        vi: "ĐĂNG KÝ DỊCH VỤ",
+        en: "GET STARTED"
+    },
+    'home=luat-su-asoka-co-kien-thuc': {
+        vi: "Luật sư Asoka có kiến thức cực kỳ chuyên sâu về lĩnh vực mình cần tham vấn, giúp mình xác nhận được những ý kiến pháp luật mình muốn tìm hiểu. Tư vấn súc tích, nhiệt tình và tin cậy. Mình cũng đã nhờ Asoka Law thực hiện các thủ tục pháp lý cho công ty riêng của mình sau đó. - Nguyễn Quang Minh, Giám đốc Thiên Kỳ Technology",
+        en: "Asoka lawyer has extremely specialized knowledge in the field that I need to consult, helping me confirm the legal opinions I want to learn. Concise advice, enthusiastic and reliable. I also asked Asoka Law to follow the legal procedures for my own company afterwards."
+    },
+    'home-asoka-law-duoc-tin-dung': {
+        vi: "Asoka Law được tin dùng bởi +3500 doanh nghiệp",
+        en: "Asoka Law is trusted by +3500 businesses"
+    },
+    'home-tu-cong-ty-khoi-nghiep': {
+        vi: "Từ công ty khởi nghiệp cho đến công ty hoạt động trên 03 năm đều dễ dàng tìm thấy các dịch vụ pháp lý thiết yếu tại Asoka Law. Đóng gói chặt chẽ theo độ tuổi doanh nghiệp/ lĩnh vực kinh doanh, tư vấn nhiệt tình, thực hiện tận tâm. Asoka Law đã hỗ trợ khách hàng kinh doanh thành công từ 2015 cho đến nay. ",
+        en: "From start-ups to companies operating for more than 3 years, it's easy to find essential legal services at Asoka Law. Closely packed according to the age of the business / business sector, enthusiastic advice, dedicated. Asoka Law has been supporting successful business customers since 2015 until now."
+    },
+    'home-khoi-tao-doanh-nghiep': {
+        vi: "Khởi tạo doanh nghiệp",
+        en: "Business establishment"
+    },
+    'home-tu': {
+        vi: "Từ",
+        en: "From"
+    },
+    'home-dich-vu-pho-bien': {
+        vi: "Dịch vụ phổ biến",
+        en: "Popular services"
+    },
+    'home-tu-van-luat-su': {
+        vi: " Tư vấn luật sư",
+        en: " Lawyer consultation"
+    },
+    'home-dangkykinhdoanh': {
+        vi: " Đăng ký kinh doanh ",
+        en: " Business registrationt"
+    },
+
+    'home-phap-ly-thuong-xuyen': {
+        vi: " Pháp lý thường xuyên ",
+        en: " Regular legal services"
+    },
+    'home-dang-ky-ngay': {
+        vi: "Đăng ký ngay",
+        en: "Get Started"
+    },
+    // denday
+    'home-doanhnghiep01-03nam': {
+        vi: "Doanh nghiệp 01 đến 03 năm",
+        en: "01-03 year businesses"
+    },
+    'homne-tu1999000k': {
+        vi: "Từ 1.990.000 VNĐ",
+        en: "From VND 1,990,000"
+    },
+    'home-dich-vu-pho-bien2': {
+        vi: "Dịch vụ phổ biến",
+        en: "Popular services"
+    },
+    'home-dang-ky-nhan-hieu': {
+        vi: "> Đăng ký nhãn hiệu",
+        en: "Trademark registration"
+    },
+    'home-giay-phep-hoat-dong': {
+        vi: "> Giấy phép hoạt động",
+        en: "Operating license"
+    },
+    'home-luu-hanh-san-pham': {
+        vi: "> Lưu hành sản phẩm",
+        en: "Product circulation"
+    },
+    'home-dang-ky-ngay2': {
+        vi: "Đăng ký ngay",
+        en: "Get Started"
+    },
+    'home-doanhnghieptren3nam': {
+        vi: "Doanh nghiệp trên 03 năm",
+        en: "Business over 3 years"
+    },
+    'home-tu199900k': {
+        vi: "Từ 2.990.000 VNĐ",
+        en: "From VND 2,990,000 "
+    },
+    'home-dichvuphobien': {
+        vi: "Dịch vụ phổ biến",
+        en: "Popular services"
+    },
+    'home-dang-ky-nhan-hieu2': {
+        vi: "Rà soát & Vá lỗi",
+        en: "Review and fix errors"
+    },
+    'home-giay-phep-hoat-dong2': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-lưu-hanh-san-pham2': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-dang-ky-ngay3': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-dn-tren-3nam': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-tu2tr9': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-dich-vu-pho-bien3': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-rasoatloi': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-xulytranhchap': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-nhuongquyen-thuongmai': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-dangkyngay': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-doi-nguluatsucovan': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-khongchi-co-luatsu': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-luatsu-tamduong': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-truyenthongsukiendoanhnghiep': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-luatsu-nguyensontra': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-tranhchapdansuthuongmai': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-86-vuviec': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+    'home-6890giotuvan': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-3500 luot-su-dung': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-hieu-qua-su-dung': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-phaplytheonhucau': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-chungtoitinrang': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-gia-phi-congkhai': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-chiphilavandesong': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-mangluoiluatsu': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-chungtoi-luachonhoptac': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-tintuc-moinhat': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-videoduocxemnhieunhat': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-goingaychochungtoi': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-asokalawla1hangluatchinhdanh': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-hosonagluc': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-phplydpanhnghiep': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-khoitaodoanhnghiep': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-dnduoi1nam': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-dnduoi3nam': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-doanhnghieptren3nam1': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-khachhang': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-cauhoithuonggap': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-chinhsachKH': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-QTlamviec': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-huongdan-thanhtoan': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-dichvuphaply': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-tu-vantructuyen': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-dangkykinhdoanh1': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+
+
+    'home-dangkynhanhieu1': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-phaplythuongxuyen3': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-phap-lysukien': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-xulytranhchap2': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-dangkynhanuudaimoinhat': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-nhapuudaimoinhat': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-nhapemailcuaban': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-gui': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-ketnoivoichungtoi': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-truso-chinh': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-chinhanh': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
+    'home-giayphepdkkd': {
+        vi: "Chọn lĩnh vực bạn quan tâm",
+        en: "Choose a topic of interest"
+    },
+
 };
